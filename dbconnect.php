bf541558a3fb9706e4b2574fbf701ec4384d9003
WeiDi <weiditan@hotmail.com>
2020-12-21 15:07:36 +0800

    update

---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "u198522155_web_project";
+$dbPassword = "8:HFMa0:o";
+$dbDatabase = "u198522155_web_project";
+
+// Create connection
+$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbDatabase);
