1dde50ee97e7aecb88344f50c2a69f07a1943930
WeiDi <weiditan@hotmail.com>
2020-12-27 00:54:17 +0800

    update

--- home.php
+++ home.php
@@ -287,12 +287,12 @@ https://templatemo.com/tm-529-ramayana
                                       userId:1
                                   }
 
-                                  fetch('https://jsonplaceholder.typicode.com/posts', {
+                                  fetch('addtask.php', {
                                       method: "POST",
                                       body: JSON.stringify(_data),
                                       headers: {"Content-type": "application/json; charset=UTF-8"}
                                   }).then(response => {
-                                      alert(response);
+                                      alert(response.text());
                                       // data is anything returned by your API/backend code
                                   });
                               }
