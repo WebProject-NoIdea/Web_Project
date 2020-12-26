a2f2788e33c7f635c4bfe120c61d8ef5ecec4570
WeiDi <weiditan@hotmail.com>
2020-12-27 00:10:25 +0800

    update

--- home.php
+++ home.php
@@ -154,7 +154,7 @@ https://templatemo.com/tm-529-ramayana
               </section>
 
               <!-- Tables -->
-              <section class="tables" style="margin-top: 0;border-top: 0;">
+              <section class="tables" style="margin-top: 0;border-top-style : none;">
                   <div class="container-fluid">
                       <div class="row">
                           <div class="col-md-12">
