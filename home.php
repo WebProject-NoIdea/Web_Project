3fd0268dc6d7f1d6c7bf77904b4ccfb56b6e9d28
WeiDi <weiditan@hotmail.com>
2020-12-27 01:19:57 +0800

    update

--- home.php
+++ home.php
@@ -294,7 +294,7 @@ https://templatemo.com/tm-529-ramayana
                                       console.log(response.text());
                                       console.log(response.status);
                                       console.log(response.ok);
-                                      location.reload();
+
                                   }).catch(error => {
                                       console.log(error);
                                   });
