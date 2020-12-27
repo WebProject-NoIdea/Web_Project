2c78868a0b1d55047ee0ffe85f750d3443b698a7
WeiDi <weiditan@hotmail.com>
2020-12-27 12:06:34 +0800

    update

--- home.php
+++ home.php
@@ -274,7 +274,7 @@ https://templatemo.com/tm-529-ramayana
                               </div>
                               <div class="modal-footer">
                                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                  <button type="submit" id="submitbtn" class="btn btn-primary" value="Save changes">
+                                  <button type="submit" id="submitbtn" class="btn btn-primary" >Save changes</button>
                               </div>
                           </form>
 
@@ -285,7 +285,7 @@ https://templatemo.com/tm-529-ramayana
                                   e.preventDefault();
 
                                   document.getElementById("submitbtn").disabled = true;
-                                  document.getElementById("submitbtn").value = "Saving...";
+                                  document.getElementById("submitbtn").innerHTML = "Saving...";
 
                                   const formData = new FormData(this);
 
