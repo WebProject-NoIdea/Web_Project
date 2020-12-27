0c54e9b9d241a09fdc5c0bea8842039ffd479f0f
WeiDi <weiditan@hotmail.com>
2020-12-27 18:36:12 +0800

    update

--- home.php
+++ home.php
@@ -386,13 +386,5 @@ https://templatemo.com/tm-529-ramayana
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <script src="assets/js/browser.min.js"></script>
-    <script src="assets/js/breakpoints.min.js"></script>
-    <script src="assets/js/transition.js"></script>
-    <script src="assets/js/owl-carousel.js"></script>
-    <script src="assets/js/custom.js"></script>
-
-
-      
 </body>
 </html>
