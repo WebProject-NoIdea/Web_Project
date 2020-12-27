e8ca14546f522791c42e7c65fcb006d9a716c96d
WeiDi <weiditan@hotmail.com>
2020-12-27 11:44:24 +0800

    update

--- home.php
+++ home.php
@@ -275,12 +275,8 @@ https://templatemo.com/tm-529-ramayana
 
                               </div>
                               <div class="modal-footer">
-                                  <!-- Button trigger modal -->
-                                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
-                                      Launch static backdrop modal
-                                  </button>
                                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                  <button type="submit" class="btn btn-primary">Save changes</button>
+                                  <button type="submit" class="btn btn-primary" onClick="this.disabled=true; this.value='Sending…';">Save changes</button>
                               </div>
                           </form>
 
@@ -310,27 +306,6 @@ https://templatemo.com/tm-529-ramayana
               </div>
 
 
-
-              <!-- Modal -->
-              <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                      <div class="modal-content">
-                          <div class="modal-header">
-                              <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                              </button>
-                          </div>
-                          <div class="modal-body">
-                              ...
-                          </div>
-                          <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                              <button type="button" class="btn btn-primary">Understood</button>
-                          </div>
-                      </div>
-                  </div>
-              </div>
 
 
 
