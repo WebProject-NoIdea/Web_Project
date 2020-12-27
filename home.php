7079fadec225a0cead88b67cba1c42f0c09c924e
WeiDi <weiditan@hotmail.com>
2020-12-27 13:22:04 +0800

    update

--- home.php
+++ home.php
@@ -136,7 +136,7 @@ https://templatemo.com/tm-529-ramayana
                                       });
 
                                       $('#endDate').datetimepicker({
-                                          value: '22:10 03/27/2018',
+                                          value: '27 Dec 2020 01:45 PM',
                                           uiLibrary: 'bootstrap4',
                                           format: 'dd mmm yyyy hh:MM TT',
                                           modal: true,
