d9ab7d811c9fd6b0fae3fa1218feb608eb073e91
WeiDi <weiditan@hotmail.com>
2020-12-31 13:07:21 +0800

    update

--- addtask.php
+++ addtask.php
@@ -6,12 +6,12 @@ checkLogin();
 if(isset($_POST['task']) AND $_POST['task']!=null){
     $task = $_POST['task'];
     $description = $_POST['description'];
-    $startDate = $_POST['startDate'];
-    $endDate = $_POST['endDate'];
+    $startDate =  date_create($_POST['startDate']);
+    $endDate = date_create($_POST['endDate']);
 
     include('dbconnect.php');
 
-    $sql = "INSERT INTO task(user_id, task, description, start_date, end_date) VALUES (".getUserId().", '$task', '$description', '$startDate', '$endDate')";
+    $sql = "INSERT INTO task(user_id, task, description, start_date, end_date) VALUES (".getUserId().", '$task', '$description', '".date_format($startDate,"Y-m-d H:i:s")."', '".date_format($endDate,"Y-m-d H:i:s")."')";
 
     if ($conn->query($sql) === false) {
         echo "Error: " . $sql . "<br>" . $conn->error;
