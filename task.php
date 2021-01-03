5f90e18d00fa55e1fca1be2c46e5f6798e438673
WeiDi <weiditan@hotmail.com>
2021-01-03 15:08:20 +0800

    update

--- task.php
+++ task.php
@@ -246,11 +246,11 @@ function diffDateInSeconds(String $datetime){
                                         </table>
                                         <ul class="table-pagination">
                                             <li id="prevBtn"><a>Previous</a></li>
-                                            <li id="btn1"><a>1</a></li>
-                                            <li id="btn2"><a>2</a></li>
-                                            <li id="btn3" class="active"><a>3</a></li>
-                                            <li id="btn4"><a>4</a></li>
-                                            <li id="btn5"><a>5</a></li>
+                                            <li><a id="btn1">1</a></li>
+                                            <li><a id="btn2">2</a></li>
+                                            <li class="active"><a id="btn3">3</a></li>
+                                            <li><a id="btn4">4</a></li>
+                                            <li><a id="btn5">5</a></li>
                                             <li id="moreBtn"><a>...</a></li>
                                             <li id="nextBtn"><a>Next</a></li>
                                         </ul>
