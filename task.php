1545196677a601e51341979d3fa9b3c105afd934
WeiDi <weiditan@hotmail.com>
2021-01-03 17:47:11 +0800

    update

--- task.php
+++ task.php
@@ -233,9 +233,82 @@ function diffDateInSeconds(String $datetime){
                                                             <td>$date</td>
                                                             <td>
                                                                 <button class='btn'><i class='fa fa-check-square-o'></i></button>
-                                                                <button class='btn''><i class='fa fa-pencil'></i></button>
+                                                                <button class='btn' data-toggle='modal' data-target='#exampleModal2'><i class='fa fa-pencil'></i></button>
+                                                
                                                             </td>
-                                                        </tr>";
+                                                        </tr>"; ?>
+
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                            <h4 class="modal-title">Add Task</h4>
+                                                        </div>
+                                                        <form id="addTaskForm">
+                                                            <div class="modal-body">
+
+                                                                <div class="form-group">
+                                                                    <label for="task">Task</label>
+                                                                    <input type="text" class="form-control" id="task" name="task" placeholder="Task" required>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label for="description">Description</label>
+                                                                    <input type="text" class="form-control" id="description" name="description" placeholder="Description" required>
+                                                                </div>
+                                                                <div class="row">
+                                                                    <div class="form-group col-sm-6">
+                                                                        <label class="control-label" for="input-datepicker-start">Start Date</label>
+                                                                        <div class="input-group" id="datepicker-start">
+                                                                            <input type="text" class="form-control" name="startDate" id="input-datepicker-start" autocomplete="off">
+                                                                            <span class="input-group-addon">
+                                                    <span class="glyphicon glyphicon-calendar"></span>
+                                                </span>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group col-sm-6">
+                                                                        <label class="control-label" for="input-datepicker-end">End Date</label>
+                                                                        <div class="input-group" id="datepicker-end">
+                                                                            <input type="text" class="form-control" name="endDate" id="input-datepicker-end" autocomplete="off">
+                                                                            <span class="input-group-addon">
+                                                    <span class="glyphicon glyphicon-calendar"></span>
+                                                </span>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <script>
+                                                                    // Linked date and time picker
+                                                                    // start date date and time picker
+                                                                    $('#datepicker-start').datetimepicker({
+                                                                        format:'ddd, DD MMM Y hh:mm A',
+                                                                    });
+
+                                                                    // End date date and time picker
+                                                                    $('#datepicker-end').datetimepicker({
+                                                                        format:'ddd, DD MMM Y hh:mm A',
+                                                                        useCurrent: false
+                                                                    });
+
+                                                                    // start date picke on chagne event [select minimun date for end date datepicker]
+                                                                    $("#datepicker-start").on("dp.change", function (e) {
+                                                                        $('#datepicker-end').data("DateTimePicker").minDate(e.date);
+                                                                    });
+                                                                    // Start date picke on chagne event [select maxmimum date for start date datepicker]
+                                                                    $("#datepicker-end").on("dp.change", function (e) {
+                                                                        $('#datepicker-start').data("DateTimePicker").maxDate(e.date);
+                                                                    });
+                                                                </script>
+
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+                                                                <button type="submit" id="submitBtn" class="btn btn-primary">Save changes</button>
+                                                            </div>
+                                                        </form>
+
+                                            <?php
 
                                                 $i++;
                                             }
