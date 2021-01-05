371df7934758b3d87e522b84992dd561103463fb
WeiDi <weiditan@hotmail.com>
2021-01-06 00:46:18 +0800

    update

--- task/updatePerfomance.php
+++ task/updatePerfomance.php
@@ -14,7 +14,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo print_r($row);
+        echo "<br>".print_r($row);
     }
 }
 
