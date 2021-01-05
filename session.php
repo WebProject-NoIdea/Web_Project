b3b904e5dd84c79904dee14f6b7652a94a2bb3ac
WeiDi <weiditan@hotmail.com>
2021-01-05 17:47:39 +0800

    update

--- session.php
+++ session.php
@@ -2,18 +2,33 @@
 session_start();
 
 function checkLogin(){
+    $urlExt = "";
+
+    $array = explode('/',$_SERVER['REQUEST_URI']);
+    if( $array[count($array)-2]!="Web_Project"){
+        $urlExt = "../";
+    }
+
     if($_SESSION['webProjectLoggedIn'] == false){
-        header( "Location: http://www.breakvoid.com/Web_Project/index.php" );
+        header( "Location: ".$urlExt."index.php" );
     }
 }
 
 function logout(){
+
+    $urlExt = "";
+
+    $array = explode('/',$_SERVER['REQUEST_URI']);
+    if( $array[count($array)-2]!="Web_Project"){
+        $urlExt = "../";
+    }
+
     // remove all session variables
     session_unset();
     // destroy the session
     session_destroy();
 
-    header( "Location: http://www.breakvoid.com/Web_Project/index.php" );
+    header( "Location: ".$urlExt."index.php" );
 }
 
 function getUserId(){
