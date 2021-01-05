a0dc3ce32667a46fc955fc4930588573d7e896a9
WeiDi <weiditan@hotmail.com>
2021-01-06 00:40:49 +0800

    update

---
+++ task/updatePerfomance.php
@@ -0,0 +1,22 @@
+<?php
+
+include("../session.php");
+checkLogin();
+
+$conn = include('../dbConnect.php');
+
+$sql = "SELECT task_id, task, description, start_date, end_date, complete_date
+                    FROM task 
+                    WHERE user_id=".getUserId()." AND complete_date != '0000-00-00 00:00:00' 
+                    ORDER BY complete_date DESC";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row;
+    }
+
+}
+
+$conn->close();
