cddd093036f9ccfbe6223b7a59a71621f4897a48
WeiDi <weiditan@hotmail.com>
2021-01-05 21:30:12 +0800

    update

--- dbconnect.php
+++ dbconnect.php
@@ -6,5 +6,4 @@ $dbPassword = "8:HFMa0:o";
 $dbDatabase = "u198522155_web_project";
 
 // Create connection
-$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbDatabase);
-?>
+return new mysqli($dbServername, $dbUsername, $dbPassword, $dbDatabase);
