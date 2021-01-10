d63eb4534bcab9f9a266d2a6b5e0c6388233a65e
WeiDi <weiditan@hotmail.com>
2021-01-10 18:46:16 +0800

    update

--- graph/statistics.php
+++ graph/statistics.php
@@ -9,7 +9,7 @@ $sql = "SELECT
             COUNT(CASE WHEN complete_date != '0000-00-00 00:00:00' AND complete_date<=end_date THEN 1 END) AS completed,
             COUNT(CASE WHEN complete_date != '0000-00-00 00:00:00' AND complete_date>end_date THEN 1 END) AS overdue,
             COUNT(CASE WHEN complete_date = '0000-00-00 00:00:00' THEN 1 END) AS in_progess
-         FROM task WHERE user_id=" . getUserId();
+         FROM task WHERE user_id=10" . getUserId();
 
 $result = $conn->query($sql);
 
