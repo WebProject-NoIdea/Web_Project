a6e980a399dca71b7e08507a9e11fce91f8b2c78
WeiDi <weiditan@hotmail.com>
2021-01-11 00:18:22 +0800

    update

--- chart/performance.php
+++ chart/performance.php
@@ -5,13 +5,13 @@ checkLogin();
 
 $conn = include('../dbConnect.php');
 
-$sql = "SELECT AVG(performance)
+$sql = "SELECT AVG(performance) AS avgPerformance
          FROM task WHERE user_id=" . getUserId();
 
 $result = $conn->query($sql);
 
 while ($row = $result->fetch_assoc()) {
-    echo json_encode(round($row["performance"],2));
+    echo json_encode(round($row["avgPerformance"],2));
 }
 
 $conn->close();
