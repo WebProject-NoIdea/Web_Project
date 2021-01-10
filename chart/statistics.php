cd32dd6a5a39ebe01bea1ec296a746edc5d92b64
WeiDi <weiditan@hotmail.com>
2021-01-10 21:36:25 +0800

    update

--- chart/statistics.php
+++ chart/statistics.php
@@ -8,7 +8,7 @@ $conn = include('../dbConnect.php');
 $sql = "SELECT 
             COUNT(CASE WHEN complete_date != '0000-00-00 00:00:00' AND complete_date<=end_date THEN 1 END) AS completed,
             COUNT(CASE WHEN complete_date != '0000-00-00 00:00:00' AND complete_date>end_date THEN 1 END) AS overdue,
-            COUNT(CASE WHEN complete_date = '0000-00-00 00:00:00' THEN 1 END) AS in_progess
+            COUNT(CASE WHEN complete_date = '0000-00-00 00:00:00' THEN 1 END) AS in_progress
          FROM task WHERE user_id=".getUserId();
 
 $result = $conn->query($sql);
