6a3dc326b4758ddbfc700194ac60d91943e7f3a2
WeiDi <weiditan@hotmail.com>
2021-01-10 23:50:55 +0800

    update

--- task/updatePerformance.php
+++ task/updatePerformance.php
@@ -3,7 +3,7 @@ class updatePerformance{
     public function update($conn,$taskId){
         $sql = "SELECT start_date, end_date, complete_date
                     FROM task 
-                    WHERE task_id=$taskId AND user_id=".getUserId();
+                    WHERE  user_id=".getUserId();
 
         $result = $conn->query($sql);
 
