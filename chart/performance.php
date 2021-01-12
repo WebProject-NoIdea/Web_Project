177f9aad2a3443040d0f6dd79f9aa5dd940c2664
WeiDi <weiditan@hotmail.com>
2021-01-13 01:41:11 +0800

    update

--- chart/performance.php
+++ chart/performance.php
@@ -13,12 +13,28 @@ class performance
     public function run()
     {
         echo $this->avgPerformance();
+        echo $this->avgPerformancePerDay();
         $this->conn->close();
     }
 
     private function avgPerformance(){
         $sql = "SELECT AVG(performance) AS avgPerformance
-         FROM task WHERE complete_date != '0000-00-00 00:00:00' AND user_id=" . getUserId();
+                FROM task 
+                WHERE complete_date != '0000-00-00 00:00:00' AND user_id=" . getUserId();
+
+        $result = $this->conn->query($sql);
+
+        $row = $result->fetch_assoc();
+
+        return json_encode(round($row["avgPerformance"],2));
+    }
+
+    private function avgPerformancePerDay(){
+        $sql = "SELECT DATE(complete_date) AS date, AVG(performance) AS avgPerformance 
+                FROM task 
+                WHERE complete_date != '0000-00-00 00:00:00' AND user_id=".getUserId()."
+                GROUP BY DATE(complete_date) 
+                ORDER BY complete_date";
 
         $result = $this->conn->query($sql);
 
