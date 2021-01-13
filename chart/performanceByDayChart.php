e3f945a171a89d4f9df2f25f969c0324b0d44aa9
WeiDi <weiditan@hotmail.com>
2021-01-14 00:07:13 +0800

    update

--- chart/performanceByDayChart.php
+++ chart/performanceByDayChart.php
@@ -1,19 +1,13 @@
 <h3>Average Performance</h3>
 
-<h4 id="performanceByDayChartTitle" style="padding-left:10px;display: inline-block;">2021 January</h4>
-<button class="btn small" id="performanceByDayChartDatepicker">Change</button>
+<h4 id="performanceByDayChartTitle" style="padding-left:10px;">2021 January</h4>
 
-<div style="overflow:hidden;">
-    <div class="form-group">
-        <div class="row">
-            <div class="col-md-8">
-                <div id="datetimepicker12"></div>
-            </div>
-        </div>
-    </div>
+
+<div id="performanceByDayChartDatepicker"></div>
+
     <script type="text/javascript">
         $(function () {
-            $('#datetimepicker12').datetimepicker({
+            $('#performanceByDayChartDatepicker').datetimepicker({
                 format:'Y MMMM',
                 inline: true,
                 sideBySide: true
@@ -21,30 +15,6 @@
         });
     </script>
 </div>
-
-<script>
-    // Linked date and time picker
-    // start date date and time picker
-    $('#performanceByDayChartDatepicker').datetimepicker({
-        format:'Y MMMM',
-        widgetPositioning:{
-            horizontal: 'auto',
-            vertical: 'bottom'
-        }
-    });
-
-    $('#dp5').datepicker()
-        .on('changeDate', function(ev){
-            if (ev.date.valueOf() < startDate.valueOf()){
-                $('#alert').show().find('strong').text('The end date can not be less then the start date');
-            } else {
-                $('#alert').hide();
-                endDate = new Date(ev.date);
-                $('#endDate').text($('#dp5').data('date'));
-            }
-            $('#dp5').datepicker('hide');
-        });
-</script>
 
 <div id="performanceByDayChartContainer" style="height: 370px; width: 100%;"></div>
 <script>
