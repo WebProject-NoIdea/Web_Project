dcffb91ef084362b0f2dae73ac0e01b66c5ff965
WeiDi <weiditan@hotmail.com>
2021-01-13 13:00:12 +0800

    update

--- chart/performanceChart.php
+++ chart/performanceChart.php
@@ -50,7 +50,8 @@
           a 15.9155 15.9155 0 0 1 0 31.831
           a 15.9155 15.9155 0 0 1 0 -31.831"
             />
-            <text id="percentage" x="18" y="20.35" class="percentage">0%</text>
+            <text id="percentage1" x="18" y="20.35" class="percentage">0&nbsp;%</text>
+            <text id="percentage2" x="18" y="20.35" class="percentage" style="font-size: 0.15em;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.00</text>
         </svg>
     </div>
     <h4 style="text-align: center;">Average Performance</h4>
@@ -61,7 +62,7 @@
 
         const avgPerformance = data.avgPerformance;
 
-        document.getElementById("percentage").innerHTML = avgPerformance+"%";
+        document.getElementById("percentage1").innerHTML = parseFloat(avgPerformance).toFixed(2)+"%";
         document.getElementById("circle").style.stroke = "#ff9f00";
         document.getElementById("circle").style.strokeDasharray = (avgPerformance/2)+", 100";
 
