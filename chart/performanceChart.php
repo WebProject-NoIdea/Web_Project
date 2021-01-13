997ac5a8f1f9aa97e15a62cb679c7602dd05d7ad
WeiDi <weiditan@hotmail.com>
2021-01-13 11:44:46 +0800

    update

--- chart/performanceChart.php
+++ chart/performanceChart.php
@@ -58,12 +58,12 @@
           a 15.9155 15.9155 0 0 1 0 -31.831"
             />
             <path class="circle"
-                  stroke-dasharray="50, 100"
+                  stroke-dasharray="0, 100"
                   d="M18 2.0845
           a 15.9155 15.9155 0 0 1 0 31.831
           a 15.9155 15.9155 0 0 1 0 -31.831"
             />
-            <text id="percentage" x="18" y="20.35" class="percentage">50%</text>
+            <text id="percentage" x="18" y="20.35" class="percentage">0%</text>
         </svg>
     </div>
     <h4 style="text-align: center;">Average Performance</h4>
