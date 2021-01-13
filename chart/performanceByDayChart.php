a8bc052285980e0b6d61aadd47e1ab2774de74a9
WeiDi <weiditan@hotmail.com>
2021-01-13 14:49:41 +0800

    update

--- chart/performanceByDayChart.php
+++ chart/performanceByDayChart.php
@@ -13,23 +13,22 @@
             data: [{
                 type: "line",
                 indexLabelFontSize: 16,
-                dataPoints: [
-                    {y: 450},
-                    {y: 414},
-                    {y: 520, indexLabel: "\u2191 highest", markerColor: "red", markerType: "triangle"},
-                    {y: 460},
-                    {y: 450},
-                    {y: 500},
-                    {y: 480},
-                    {y: 480},
-                    {y: 410, indexLabel: "\u2193 lowest", markerColor: "DarkSlateGrey", markerType: "cross"},
-                    {y: 500},
-                    {y: 480},
-                    {y: 510}
-                ]
+                dataPoints: performanceByDayChartDataPoints
             }]
         });
 
-        performanceByDayChart.render();
+        function performanceByDayChartAddData(data) {
+
+            for (let i = 0; i < data.length; i++) {
+                performanceByDayChartDataPoints.push({
+                    x: new Date(data[i].date),
+                    y: data[i].avgPerformance
+                });
+            }
+
+            performanceByDayChart.render();
+        }
+
+        $.getJSON("chart/performance.php", performanceByDayChartAddData);
     }
 </script>
