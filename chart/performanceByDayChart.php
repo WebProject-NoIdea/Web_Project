795889a350fa977aa0309ce8504c2cdc229465d5
WeiDi <weiditan@hotmail.com>
2021-01-14 00:04:29 +0800

    update

--- chart/performanceByDayChart.php
+++ chart/performanceByDayChart.php
@@ -3,6 +3,24 @@
 <h4 id="performanceByDayChartTitle" style="padding-left:10px;display: inline-block;">2021 January</h4>
 <button class="btn small" id="performanceByDayChartDatepicker">Change</button>
 
+<div style="overflow:hidden;">
+    <div class="form-group">
+        <div class="row">
+            <div class="col-md-8">
+                <div id="datetimepicker12"></div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        $(function () {
+            $('#datetimepicker12').datetimepicker({
+                inline: true,
+                sideBySide: true
+            });
+        });
+    </script>
+</div>
+
 <script>
     // Linked date and time picker
     // start date date and time picker
@@ -13,6 +31,18 @@
             vertical: 'bottom'
         }
     });
+
+    $('#dp5').datepicker()
+        .on('changeDate', function(ev){
+            if (ev.date.valueOf() < startDate.valueOf()){
+                $('#alert').show().find('strong').text('The end date can not be less then the start date');
+            } else {
+                $('#alert').hide();
+                endDate = new Date(ev.date);
+                $('#endDate').text($('#dp5').data('date'));
+            }
+            $('#dp5').datepicker('hide');
+        });
 </script>
 
 <div id="performanceByDayChartContainer" style="height: 370px; width: 100%;"></div>
