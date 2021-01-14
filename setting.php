5530f6ceff70ff9021325ae9ab1c5ce8672e3f08
WeiDi <weiditan@hotmail.com>
2021-01-14 13:17:07 +0800

    update

--- setting.php
+++ setting.php
@@ -84,19 +84,26 @@ $conn->close();
 
             <div class="row">
                 <div class="col-md-6">
-                    <form>
+                    <form action="" method="post">
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            <label for="firstName">First Name</label>
+                            <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First Name">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            <label for="lastName">Last Name</label>
+                            <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last Name">
                         </div>
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <div class="form-group">
+                            <label for="oldPassword">Old Password</label>
+                            <input type="password" class="form-control" name="oldPassword" id="oldPassword" placeholder="Old Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="newPassword1">New Password</label>
+                            <input type="password" class="form-control" name="newPassword1" id="newPassword1" placeholder="New Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="newPassword2">Confirm New Password</label>
+                            <input type="password" class="form-control" name="newPassword2" id="newPassword2" placeholder="Confirm New Password">
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
