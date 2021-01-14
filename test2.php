1bdf4056eec2097186482bf40f0d67edfaf52d7a
WeiDi <weiditan@hotmail.com>
2021-01-14 12:31:24 +0800

    update

--- test2.php
+++ test2.php
@@ -113,8 +113,9 @@ checkLogin();
 
         $.getJSON("task/getTask.php?type=history", function (data){
             data.forEach(function (item) {
+
                 const row = `<tr>
-                                    <td onclick="view(${item})">#${i}</td>
+                                    <td onclick="view(${JSON.stringify(item)})">#${i}</td>
                                     <td>${item.task}</td>
                                     <td>${item.description}</td>
                                     <td>${item.complete_date}</td>
