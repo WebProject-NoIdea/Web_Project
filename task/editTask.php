065579398c47d4722f2f11f1e567b7aed4810308
WeiDi <weiditan@hotmail.com>
2021-01-14 15:55:04 +0800

    update

--- task/editTask.php
+++ task/editTask.php
@@ -10,6 +10,13 @@ if(isset($_POST['taskId']) AND $_POST['taskId']!=null){
     $startDate =  date_create($_POST['startDate']);
     $endDate = date_create($_POST['endDate']);
 
+    $sqlCompleteDate = "";
+
+    if(isset($_POST['completeDate'])){
+        $completeDate = date_create($_POST['completeDate']);
+        $sqlCompleteDate = ",complete_date='".date_format($completeDate,"Y-m-d H:i:s")."'";
+    }
+
     $conn = include('../dbConnect.php');
 
     $sql = "UPDATE task
@@ -17,11 +24,16 @@ if(isset($_POST['taskId']) AND $_POST['taskId']!=null){
                     description='$description',
                     start_date='".date_format($startDate,"Y-m-d H:i:s")."',
                     end_date='".date_format($endDate,"Y-m-d H:i:s")."'
+                    $sqlCompleteDate
             WHERE task_id=$taskId AND user_id=".getUserId();
 
     if ($conn->query($sql) === false) {
         echo "Error: " . $sql . "<br>" . $conn->error;
+    }elseif (isset($_POST['completeDate'])){
+        include('updatePerformance.php');
+        (new updatePerformance)->update($conn,$taskId);
     }
+
 
     $conn->close();
 }
